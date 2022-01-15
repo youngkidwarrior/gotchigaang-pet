5244a32b34b91a968f8f0b2faefbad7e5c1c932b
VictorGinelli <ginelliunit@gmail.com>
2022-01-15 06:52:13 -0800

    Allow for owners to be added and removed

--- packages/contracts/contracts/LazyPetter.sol
+++ packages/contracts/contracts/LazyPetter.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.0;
 
-import { PokeMeReady } from "./PokeMeReady.sol";
+import { PokeMeReady } from './PokeMeReady.sol';
 
 interface AavegotchiFacet {
   function tokenIdsOfOwner(address _owner)
     external
     view
     returns (uint32[] memory tokenIds_);
+
+  function setPetOperatorForAll(address _operator, bool _approved) external;
 }
 
 interface AavegotchiGameFacet {
@@ -20,6 +22,9 @@ contract LazyPetter is PokeMeReady {
   AavegotchiFacet private af;
   AavegotchiGameFacet private agf;
 
+  event AddGotchiOwners(address[] gotchiOwners);
+  event RemoveGotchiOwners(address[] gotchiOwners);
+
   constructor(
     address payable _pokeMe,
     address gotchiDiamond,
@@ -30,6 +35,28 @@ contract LazyPetter is PokeMeReady {
     gotchiOwners = _gotchiOwners;
   }
 
+  function addGotchiOwners(address[] _gotchiOwners) external {
+    for (uint256 i = 0; i < _gotchiOwners.length; i++) {
+      uint32[] memory gotchis = af.tokenIdsOfOwner(gotchiOwners[i]);
+      require(gotchis.length > 0, 'Addresses must have at least one gotchi');
+      af.setPetOperatorForAll(address(this), true);
+      gotchiOwners.push(_gotchiOwners[i]);
+    }
+    emit AddGotchiOwners(_gotchiOwners);
+  }
+
+  function removeGotchiOwners(address[] _gotchiOwners) external {
+    for (uint256 i = 0; i < _gotchiOwners.length; i++) {
+      for (uint256 j = 0; j < gotchiOwners.length; j++) {
+        if (gotchiOwners[j] == _gotchiOwners[i]) {
+          af.setPetOperatorForAll(address(this), false);
+          delete gotchiOwners[j];
+        }
+      }
+    }
+    emit RemoveGotchiOwners(_gotchiOwners);
+  }
+
   function petGotchis() external onlyPokeMe {
     require(
       ((block.timestamp - lastExecuted) > 43200),
@@ -37,11 +64,11 @@ contract LazyPetter is PokeMeReady {
     );
     for (uint256 i = 0; i < gotchiOwners.length; i++) {
       uint32[] memory gotchis = af.tokenIdsOfOwner(gotchiOwners[i]);
-    uint256[] memory gotchiIds = new uint256[](gotchis.length);
+      uint256[] memory gotchiIds = new uint256[](gotchis.length);
       for (uint256 i = 0; i < gotchis.length; i++) {
-      gotchiIds[i] = uint256(gotchis[i]);
-    }
-    agf.interact(gotchiIds);
+        gotchiIds[i] = uint256(gotchis[i]);
+      }
+      agf.interact(gotchiIds);
     }
 
     lastExecuted = block.timestamp;
