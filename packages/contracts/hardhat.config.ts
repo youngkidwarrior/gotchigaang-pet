d86f0faec596b9c841a745e364b7e5d573331626
VictorGinelli <ginelliunit@gmail.com>
2022-01-15 07:10:15 -0800

    Change config to typescript

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,21 +1,11 @@
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "hardhat-contract-sizer";
-import "solidity-coverage";
-import * as dotenv from "dotenv";
-import "@typechain/hardhat";
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-contract-sizer';
+import 'solidity-coverage';
+import * as dotenv from 'dotenv';
+import '@typechain/hardhat';
 
-dotenv.config({ path: "./../../.env" });
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+dotenv.config({ path: './../../.env' });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -23,7 +13,7 @@ task('accounts', 'Prints the list of accounts', async () => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+export default {
   networks: {
     hardhat: {
       forking: {
@@ -39,6 +29,16 @@ module.exports = {
     localhost: {
       url: 'http://localhost:8545',
     },
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    enabled: false,
+  },
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: false,
+    disambiguatePaths: true,
   },
   solidity: {
     version: '0.8.0',
