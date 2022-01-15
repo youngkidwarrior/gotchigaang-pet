f95068a1d3472f10de5d4fae702756184cfef785
VictorGinelli <ginelliunit@gmail.com>
2022-01-15 08:39:29 -0800

    Use Web3Modal hook

---
+++ packages/web/hooks/useWeb3Modal.js
@@ -0,0 +1,145 @@
+import Web3Modal from 'web3modal';
+import { providers } from 'ethers';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { useReducer, useEffect, useCallback } from 'react';
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: process.env.INFURA_ID, // required
+    },
+  },
+};
+
+let web3Modal;
+if (typeof window !== 'undefined') {
+  web3Modal = new Web3Modal({
+    network: 'mainnet', // optional
+    cacheProvider: true,
+    providerOptions, // required
+  });
+}
+
+const initialState = {
+  provider: null,
+  web3Provider: null,
+  address: null,
+  chainId: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_WEB3_PROVIDER':
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        chainId: action.chainId,
+      };
+    case 'SET_ADDRESS':
+      return {
+        ...state,
+        address: action.address,
+      };
+    case 'SET_CHAIN_ID':
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    case 'RESET_WEB3_PROVIDER':
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
+
+
+const useWeb3Modal = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { provider, web3Provider, address, chainId } = state;
+
+  const connect = useCallback(async function () {
+    // This is the initial `provider` that is returned when
+    // using web3Modal to connect. Can be MetaMask or WalletConnect.
+    const provider = await web3Modal.connect();
+
+    // We plug the initial `provider` into ethers.js and get back
+    // a Web3Provider. This will add on methods from ethers.js and
+    // event listeners such as `.on()` will be different.
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const signer = web3Provider.getSigner();
+    const address = await signer.getAddress();
+
+    const network = await web3Provider.getNetwork();
+
+    dispatch({
+      type: 'SET_WEB3_PROVIDER',
+      provider,
+      web3Provider,
+      address,
+      chainId: network.chainId,
+    });
+  }, []);
+
+  const disconnect = useCallback(
+    async function () {
+      await web3Modal.clearCachedProvider();
+      if (provider?.disconnect && typeof provider.disconnect === 'function') {
+        await provider.disconnect();
+      }
+      dispatch({
+        type: 'RESET_WEB3_PROVIDER',
+      });
+    },
+    [provider]
+  );
+
+
+
+  // A `provider` should come with EIP-1193 events. We'll listen for those events
+  // here so that when a user switches accounts or networks, we can update the
+  // local React state with that new information.
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts) => {
+        // eslint-disable-next-line no-console
+        console.log('accountsChanged', accounts);
+        dispatch({
+          type: 'SET_ADDRESS',
+          address: accounts[0],
+        });
+      };
+
+      // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
+      const handleChainChanged = (_hexChainId) => {
+        window.location.reload();
+      };
+
+      const handleDisconnect = (error) => {
+        // eslint-disable-next-line no-console
+        console.log('disconnect', error);
+        disconnect();
+      };
+
+      provider.on('accountsChanged', handleAccountsChanged);
+      provider.on('chainChanged', handleChainChanged);
+      provider.on('disconnect', handleDisconnect);
+
+      // Subscription Cleanup
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener('accountsChanged', handleAccountsChanged);
+          provider.removeListener('chainChanged', handleChainChanged);
+          provider.removeListener('disconnect', handleDisconnect);
+        }
+      };
+    }
+  }, [provider, disconnect]);
+
+  return { web3Modal, connect, disconnect, web3Provider, address}
+}
+export default useWeb3Modal;
+ 
--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import useWeb3Modal from '/hooks/useWeb3Modal';
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const { web3Modal, connect } = useWeb3Modal();
+  // Auto connect to the cached provider
+  useEffect(() => {
+    if (web3Modal && web3Modal.cachedProvider) {
+      connect();
+    }
+  }, [connect, web3Modal]);
   return <Component {...pageProps} />
 }
 
